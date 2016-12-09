d3ba3fa24f79849a3cbba3eac2dfa5454b94f1c2
Darin Thompson <Darin Thompson>
2016-12-09 11:13:43 -0800

    made progress on displaying content

--- app.js
+++ app.js
@@ -1,31 +1,19 @@
 var state = {
-  items: ['apples', 'oranges', 'shwarma']
+  list: ['oranges', 'apples', 'shwarma']
 };
 
 function addItem(state, item) {
-  state.items.push(item);
-  console.log(state);
-};
+  state.list.push(item);
+}
 
-function checkItem(item) {
-  $(item).toggleClass('checked');
-};
+function displayList(state) {
+  for (var i=0; i < state.list.length; i++) {
+    var list = $(".items").append('<li>' + state.list[i] + '</li>');
+  }
+  return list;
+}
 
-function delteItem(state, index) {
- state.items.splice(index, 1); 
-};
-
-function renderState(state) {
-  for (var i=0; i < state.items.length; i++) {
-      var list_item = '';
-};
-
-$(document).ready(function (event) {
-    $('#js-shopping-list-form').on('submit', function(event) {
-    event.preventDefault();
-    var shopping_list_item = $('#shopping-list-entry').val();
-    addItem(state, shopping_list_item);
-    });
+$("#js-shopping-list-form").on('submit', function(event) {
+  event.preventDefault();
+  addItem(state, $("#shopping-list-entry").val());
 });
-
-
